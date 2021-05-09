f30c828e9389a49efce3a3069a872c6ebd8ef88f
Rico Roy Adonis Relos Reyes <1244-19@itmlyceumalabang.onmicrosoft.com>
2021-05-08 22:33:14 -1200

    E3MA123

---
+++ navbar/src/nabvar.js
@@ -0,0 +1,12 @@
+const Nav  = () =>{
+    return (  
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+   
+   )
+}
+
+export default Nav;
